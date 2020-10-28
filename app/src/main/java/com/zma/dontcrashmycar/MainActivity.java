e130ed76e8172e8880afc809da072e400b1f850f
Zacharie <zbaril@orange.fr>
2020-10-28 15:57:08 +0100

    sur ma branche??

--- app/src/main/java/com/zma/dontcrashmycar/MainActivity.java
+++ app/src/main/java/com/zma/dontcrashmycar/MainActivity.java
@@ -1,13 +1,9 @@
 package com.zma.dontcrashmycar;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,29 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    /**
-     * Simply launch the activity GameActivity in order to start a new game.
-     */
-    public void play(View view) {
-        Intent intent = new Intent(this, GameActivity.class);
-        startActivity(intent);
-        finish();
-    }
-
-    /**
-     * Go to the CarChooserActivity
-     */
-    public void chooseCar(View view) {
-        Intent intent = new Intent(this, CarChooserActivity.class);
-        startActivity(intent);
-    }
-
-    /**
-     * Go to the scores activity (with empty intent, because we don't have registered any new score)
-     */
-    public void seeScores(View view) {
-
+        Log.i("Test", "je préferais Pro Evolution Car");
+        Log.i("new test", "ça marche ou ça marche pas...");
     }
 }
