7c926dbb80fb98f10a74dcc5d9ccd45c538d46f9
Maxence <maxence.hennekein@orange.fr>
2020-11-19 15:53:12 +0100

    des voitures alignés, prochaine modif pour les ajouter depuis l'activity
    Signed-off-by: Maxence <maxence.hennekein@orange.fr>

--- app/src/main/java/com/zma/dontcrashmycar/CarChooserActivity.java
+++ app/src/main/java/com/zma/dontcrashmycar/CarChooserActivity.java
@@ -1,38 +1,32 @@
 package com.zma.dontcrashmycar;
 
 import android.os.Bundle;
+import android.widget.GridLayout;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import static com.zma.dontcrashmycar.R.*;
 
 public class CarChooserActivity extends AppCompatActivity {
-    LinearLayout carList, carSubList;
+    GridLayout carList;
     ImageButton ImgButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_carchooser);
-        showCarList();
+        //showCarList();
     }
 
     private void showCarList() {
         carList = findViewById(id.carChooser);
         for (int i = 0; i < 12; i++) {
-            if (i % 3 == 0) {
-                carSubList = new LinearLayout(this);
-            }
             ImgButton = new ImageButton(this);
             ImgButton.setImageResource(drawable.car);
             ImgButton.setTag(i);
-            carSubList.addView(ImgButton);
-            if (i % 3 == 2) {
-                carList.addView(carSubList);
-                carSubList = null;
-            }
+            carList.addView(ImgButton);
         }
 
     }
